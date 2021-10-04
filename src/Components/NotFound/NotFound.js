20cdcb200326d955932c8578fe0db3e5fee1825e
Ashikur Rahman <smashikurrahman24@gmail.com>
2021-10-04 22:18:39 +0600

    Update

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
     return (
         <div>
             <h1>404</h1>
-            <h4>Page is not found</h4>
+            <h4>Page not found</h4>
         </div>
     );
 };
